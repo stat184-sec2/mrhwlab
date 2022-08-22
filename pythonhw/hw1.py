ddfd44507fd357d8c28e0f2b5bfd637da614226c
Daniel Kifer <dkifer@cse.psu.edu>
2022-08-22 13:24:39 -0400

    hw1.py

---
+++ pythonhw/hw1.py
@@ -0,0 +1,30 @@
+
+
+def q1(mystring):
+    """ split the string by tabs to get an array and return the array """
+    pass
+
+
+def q2(mystring):
+    """ split the string by tabs to get an array and return the second element of the array """
+    pass
+
+
+
+def q3(myarray):
+    """ myarray is an list of pairs. this function should return the sum of the first
+    items in the pair and the sum of the second items """
+    pass
+
+
+def q4(mystringarray):
+    """ return the position of the first occurrence of the string 'hi' or -1 if it is not found.
+    you cannot change how the array is iterated and you cannot use any list operations on mystringarray"""
+
+    for mystring in mystringarray:
+        pass
+
+
+def q5(myarray):
+    """ return a dictionary containing the counts of items in the input array """
+    pass
