e70d51db1200e26f26e8c7921a7d1c4189af1fbc
Daniel Kifer <dkifer@cse.psu.edu>
2022-11-14 14:41:01 -0500

    spark hw

---
+++ sparkhw/q1/q1.scala
@@ -0,0 +1,10 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+object Q1 extends App {
+  def main(args: Array[String]) = {
+    // call your code using spark-submit nameofjarfile.jar commandlinearg
+    val cmd_arg = args(0) // this is the commandlinearg
+  }
+}
