f11e6b2acccf19128fba01079dc86657b7505c52
Daniel Kifer <dkifer@cse.psu.edu>
2023-09-05 14:18:14 -0400

    dask homework

---
+++ daskhw/daskhw.py
@@ -0,0 +1,13 @@
+
+
+
+# remember to have informative comments about how your functions work. avoid captain obvious comments
+def fibserial(num, base):
+    pass # `pass' does nothing, it is just useful when python requires something on a line, like for an empty function
+
+
+def fibparallel(num, base):
+    pass
+
+
+# make sure to create testing functions.
