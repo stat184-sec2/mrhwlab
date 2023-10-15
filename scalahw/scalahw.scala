5d097eb11e3c0676e0ec4fb3545e2d88e4c95466
Daniel Kifer <dkifer@cse.psu.edu>
2023-10-15 15:54:11 -0400

    scala hw

---
+++ scalahw/scalahw.scala
@@ -0,0 +1,38 @@
+case class Neumaier(sum: Double, c: Double)
+
+object HW {
+
+   def q1_countsorted(x: Int, y: Int, z:Int) = {
+      //the types of the input parameters have been declared.
+      //you must do the same for the output type (see scala slides)
+      //do not use return statements.
+      -1
+   }
+
+   def q2_interpolation(name: String, age: Int) = {
+      //the types of the input parameters have been declared.
+      //you must do the same for the output type (see scala slides)
+      //do not use return statements.
+      ""
+   }
+
+   def q3_polynomial(arr: Seq[Double]) = {
+      //the types of the input parameters have been declared.
+      //you must do the same for the output type (see scala slides)
+      //do not use return statements.
+      0.0
+   }
+
+   def q4_application(x: Int, y: Int, z: Int)(f: (Int, Int) => Int) = {
+      //the types of the input parameters have been declared.
+      //you must do the same for the output type (see scala slides)
+      //do not use return statements.
+      1
+   }
+
+   // create the rest of the functions yourself
+   // in order for the code to compile, you need to (at the very least) create
+   // blank versions of the remaining functions and have them return a value of 
+   // the expected type, like the blank functions above.
+   // remember, to compile, you don't specify any file names, you just use sbt compile
+}
